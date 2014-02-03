6a078a3413d5b882b5e791aea9010acc4bfbd3dd
fenyo1 <fenyo.pub+github@gmail.com>
2014-02-03 15:21:33 +0100

    Introducing a new class: LayoutManagerState

--- src/Battlescape/InventoryState.cpp
+++ src/Battlescape/InventoryState.cpp
@@ -34,6 +34,7 @@
 #include "../Savegame/BattleUnit.h"
 #include "../Savegame/Soldier.h"
 #include "../Savegame/EquipmentLayoutItem.h"
+#include "LayoutManagerState.h"
 #include "Inventory.h"
 #include "../Ruleset/Ruleset.h"
 #include "../Ruleset/RuleItem.h"
