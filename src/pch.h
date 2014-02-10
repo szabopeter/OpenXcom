492d2b6618dccf792f818a2fde951c22ac71d21d
fenyo1 <fenyo.pub+github@gmail.com>
2014-02-10 04:59:04 +0100

    Introducing a new class: EquipmentLayout

--- src/pch.h
+++ src/pch.h
@@ -354,6 +354,7 @@
 #include "./Savegame/SerializationHelper.h"
 #include "./Savegame/Soldier.h"
 #include "./Savegame/AlienStrategy.h"
+#include "./Savegame/EquipmentLayout.h"
 #include "./Savegame/EquipmentLayoutItem.h"
 
 #endif
