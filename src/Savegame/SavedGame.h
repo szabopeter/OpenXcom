94cda06dc707d737ed9513437ba533b66b9ca052
fenyo1 <fenyo.pub+github@gmail.com>
2014-02-15 05:54:41 +0100

    Making the logic behind the Layout Manager.

--- src/Savegame/SavedGame.h
+++ src/Savegame/SavedGame.h
@@ -47,6 +47,7 @@ class AlienStrategy;
 class AlienMission;
 class Target;
 class Soldier;
+class EquipmentLayout;
 
 /**
  * Enumerator containing all the possible game difficulties.
@@ -79,9 +80,11 @@ private:
 	std::vector<int> _funds, _maintenance, _researchScores, _incomes, _expenditures;
 	double _globeLon, _globeLat;
 	int _globeZoom;
+	int _newSoldierLayoutId;
 	std::map<std::string, int> _ids;
 	std::vector<Country*> _countries;
 	std::vector<Region*> _regions;
+	std::vector<EquipmentLayout*> _layouts;
 	std::vector<Base*> _bases;
 	std::vector<Ufo*> _ufos;
 	std::vector<Waypoint*> _waypoints;
@@ -138,6 +141,10 @@ public:
 	int getGlobeZoom() const;
 	/// Sets the new globe zoom.
 	void setGlobeZoom(int zoom);
+	/// Gets the layout Id of the layout which is automatically assigned to a new soldier.
+	int getNewSoldierLayoutId() const;
+	/// Sets the layout Id of the layout which is automatically assigned to a new soldier.
+	void setNewSoldierLayoutId(int id);
 	/// Handles monthly funding.
 	void monthlyFunding();
 	/// Gets the current game time.
@@ -152,6 +159,12 @@ public:
 	int getCountryFunding() const;
 	/// Gets the list of regions.
 	std::vector<Region*> *getRegions();
+	/// Gets the list of layouts.
+	std::vector<EquipmentLayout*> *getLayouts();
+	/// Gets the list of layouts.
+	const std::vector<EquipmentLayout*> *getLayouts() const;
+	/// Gets the layout matching this ID.
+	EquipmentLayout *getLayout(int id) const;
 	/// Gets the list of bases.
 	std::vector<Base*> *getBases();
 	/// Gets the list of bases.
